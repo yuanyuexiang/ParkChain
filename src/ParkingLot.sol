0308ecb088e70c6df82eb16f71cfde8e5fbc8259
yuanyuexiang <yuanyuexiang@126.com>
2025-01-13 18:24:15 +0800

    update parking lot contract

--- src/ParkingLot.sol
+++ src/ParkingLot.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
